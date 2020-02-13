640608d5d96b4d9003e5eeef97e031279405b946
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-13 12:02:57 -0800

    implemented the moment dependency to display dates in js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const moment = require('moment')
 const members = require('./Members')
 
 const app = express();
@@ -12,7 +13,7 @@ app.get('/api/members', (req, res) => {
 
 // a simple middlewar function
 const logger = (req, res, next) => {
-  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`) // this will give us the http
+  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}:${moment().format()}`) // this will give us the whole url that it sends
   next();
 }
 
