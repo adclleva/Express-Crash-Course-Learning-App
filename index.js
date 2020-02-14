b5090af88d123ed048d756efd9d5fdb00bfa7230
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-13 17:16:26 -0800

    Able to utilize handlebars to get data and display them on a a view page

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+// const logger = require('./middleware/logger') -> wont don't really need this, it was jsut an example of using middleware
 const express = require('express');
 const path = require('path');
-const logger = require('./middleware/logger')
+const exphbs = require('express-handlebars')
+const members = require("./Members.js")
+
 const app = express();
+
+/* Handlebars Middleware */
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Body Parser Middleware
 app.use(express.json())
 // this will handle form submissions; url encoded data
 app.use(express.urlencoded({ extended: false }));
+
+/**
+ * the Homepage Route is what renders first before the static files
+ */
+
+// Homepage Route
+app.get('/', (req, res) => res.render('index', {
+  title: "Member App",
+  members: members
+}))
 
 // this is to serve static files
 // Setting the static folder
